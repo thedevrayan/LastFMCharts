6f421542bfaa933df263cc353965fda9306de18e
Rayan Santos <rayansantos725@gmail.com>
2025-11-22 22:28:17 -0300

    server.ts atualizado para se conectar ao backend na nuvem

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,21 @@
 import express from 'express';
-import { Router } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import playlistRoutes from './routes/playlistRoutes';
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use('/api', playlistRoutes);
 
+const PORT = process.env.PORT || 3000;
+
 mongoose.connect("mongodb+srv://lastfmUser:Lastfm123!@cluster0.oeizf0j.mongodb.net/lastfmDB?retryWrites=true&w=majority")
   .then(() => {
     console.log('✅ MongoDB Atlas conectado!');
-    app.listen(3000, '0.0.0.0', () => console.log('Servidor rodando em http://0.0.0.0:3000'));
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor rodando na porta ${PORT}`);
+    });
   })
   .catch(err => console.error('❌ Erro ao conectar no MongoDB Atlas:', err));
-
